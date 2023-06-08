a9d59d608e3050f8f3d1d727108501eee2160de8
CEO OF STOICISM <nikola.lukov.2006@gmail.com>
2023-06-08 19:40:04 +0300

    Implementation of register page

---
+++ src/controllers/userController.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/register', (req,res) => {
+    res.render('user/register');
+});
+
+module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
+const userController = require('./controllers/userController');
 const cubeController = require('./controllers/cubeController');
 const accessoryController = require('./controllers/accessoryController');
 
 router.use(homeController);
-router.use('/accessories',accessoryController);
+router.use('/user',userController);
 router.use('/cube',cubeController);
+router.use('/accessories',accessoryController);
 
 router.get('*',(req,res) => {
     res.render('404');
